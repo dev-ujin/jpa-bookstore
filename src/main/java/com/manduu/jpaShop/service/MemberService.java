4ba91b145e6cc4837c7e1a72f0df3edfa48a64d5
YouJin Lee <dev.ujin518@gmail.com>
2020-11-18 02:25:12 +0900

    Complete MemberService

--- src/main/java/com/manduu/jpaShop/service/MemberService.java
+++ src/main/java/com/manduu/jpaShop/service/MemberService.java
@@ -1,4 +1,40 @@
 package com.manduu.jpaShop.service;
 
+import com.manduu.jpaShop.domain.Member;
+import com.manduu.jpaShop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
 public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Already exists.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOnd(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
 }
