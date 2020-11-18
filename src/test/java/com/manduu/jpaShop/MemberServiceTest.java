d7a056f5d32c8845b489e00b0cb0fa074845a606
YouJin Lee <dev.ujin518@gmail.com>
2020-11-18 20:17:28 +0900

    Test: Exception test

--- src/test/java/com/manduu/jpaShop/MemberServiceTest.java
+++ src/test/java/com/manduu/jpaShop/MemberServiceTest.java
@@ -24,7 +24,9 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    /* DB On mode
     @Rollback(false)
+    **/
     public void 회원가입() throws Exception {
         //given
         Member member = new Member();
@@ -37,14 +39,20 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(saveId));
     }
     
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
-        
-        
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
         //when
-        
-        
+        memberService.join(member1);
+        memberService.join(member2);
+
         //then
+        fail("예외가 발생해야 한다.");
     }
 }
